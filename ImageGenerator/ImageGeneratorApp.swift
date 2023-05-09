a1d9cea544c9de49d605cd6a816f9ca8af6c51bc
Brenda SC <brenda.saavedra@fondeadora.com>
2023-05-09 13:57:50 -0600

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by Brenda Saavedra  on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
